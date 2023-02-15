2d106cf2b04eabd6ecba3d095cb5219b9f7706d9
Gino <ginococeres2000@gmail.com>
2023-02-15 14:10:49 -0300

    Se agrego manipulacion del DOM con js

--- script.js
+++ script.js
@@ -39,3 +39,60 @@ function toggleCartAside () {
    aside.classList.toggle("inactive");
 }
 
+const productList = [];
+productList.push({
+  name: "Bike",
+  price: 120,
+  image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&w=400"
+});
+productList.push({
+  name: "Pantalla",
+  price: 220,
+  image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&w=400"
+});
+productList.push({
+  name: "Computadora",
+  price: 620,
+  image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&w=400"
+});
+
+/* <div class="product-card">
+<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&w=400" alt="" class="product-img">
+<div class="product-info">
+  <div>
+    <p>$120,00</p>
+    <p>Bike</p>
+  </div>
+  <figure>
+    <img src="./Assets/Icons/bt_add_to_cart.svg" alt="">
+  </figure>
+</div>
+</div> */
+
+for (product of productList) {
+  const productCard = document.createElement("div");
+  productCard.classList.add("product-card");
+
+  const productImg = document.createElement("img");
+  productImg.setAttribute("src", product.image);
+
+  const productInfo = document.createElement("div");
+  productInfo.classList.add("product-info");
+
+  const productPrice = document.createElement("p");
+  productPrice.innerText = "$" + product.price;
+  const productName = document.createElement("p");
+  productName.innerText = product.name;
+
+  productInfoDiv.append(productPrice, productName);
+
+  const productInfoFigure = document.createElement("figure");
+  const productImgCart = document.createElement("img");
+  img.setAttribute("src", "./Assets/Icons/bt_add_to_cart.svg");
+
+  productInfoFigure.appendChild(productImgCart);
+
+  productInfo.append(productInfoDiv, productInfoFigure);
+
+  productCard.append(productImg, productInfo);
+}
